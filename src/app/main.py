4bf334236a88f2277fbc712ed532e473cdbb0320
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-05-25 18:32:31 -0300

    feat: adiciona rota history

--- src/app/main.py
+++ src/app/main.py
@@ -193,6 +193,9 @@ def withdraw(request: dict):
     transaction = transacao_repo.add_transaction(transacao)
     return transacao.to_dict()
 
+@app.get("/history/")
+def get_history():
+    return transacao_repo.get_all_transactions_to_dict()
 
 
 handler = Mangum(app, lifespan="off")
